e01795910b51a8fe86f328e21b31074571d76d70
MarshaZheng <2001210557@stu.pku.edu.cn>
2021-01-06 23:00:35 +0800

    添加后台和前台的菜单

---
+++ application/user/controller/Main.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\user\controller;
+use think\Controller;
+use think\Loader;
+use app\user\controller\Common;
+class Main extends Controller {
+
+
+    public function index()
+    {
+        return view();
+    }
+
+
+
+    
+}
